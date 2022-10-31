8161da1e6c41ea3a7409f1c7e029fd68510e220f
Gustavo Valenzuela <GALTdea@gmail.com>
2022-10-31 12:01:33 -0700

    added users controller, route resources method for users, and views for index, show, edit, new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/show'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'home/show'
+
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
