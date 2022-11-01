1b79e8c6dc10b2bab66ef48ecbb308817aebd312
Gustavo Valenzuela <GALTdea@gmail.com>
2022-10-31 18:28:47 -0700

    create users controller, index, show, new views)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,39 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) 
+
+    respond_to do |format|
+      if @user.save
+        format.html{ redirect_to @user, notice: 'User was successfully created.' }
+        format.json{ render :show, status: :created, location: @user }
+      else
+        format.html{ render :new }
+        format.json{ render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
   end
+
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, )
+  end
+
+
 end
